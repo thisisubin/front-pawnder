4259886ab341d720eda50db762204bdde57ae1e7
thisisubin <bm1418109@gmail.com>
2025-08-17 15:18:31 +0900

    배포

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 // 환경에 따른 API 기본 URL 설정
-const API_BASE_URL = process.env.REACT_APP_API_URL || '';
+const API_BASE_URL =  'https://www.pawnder.site';
 
 // axios 인스턴스 생성
 const api = axios.create({
-    baseURL: API_BASE_URL, //https로 변경
+    baseURL: API_BASE_URL, //https로 변경했음
     withCredentials: true,
     timeout: 10000,
 });
